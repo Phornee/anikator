b3c695bfdc1bead75f3316fa613e9c1f977f3e33
Phornee <phornee@gmail.com>
2023-01-15 20:35:27 +0100

    Refactor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # anikator
+Feature:
+* Que cuando no adivine el animal, pregunte una lista de caracteristicas de los dos (guessed & user)
--- anikator.py
+++ anikator.py
@@ -1,13 +1,15 @@
 import copy
 import yaml
 import random
+import shutil
 from yaml.loader import SafeLoader
+
 
 def calc_best_tag(current_animals: list):
     # Count the number of uses of each tag
     tag_count = {}
-    for animal in current_animals:
-        for tag_select in animal['tags']:
+    for key, value in current_animals.items():
+        for tag_select in value['tags']:
             if tag_select not in tag_count:
                 tag_count[tag_select] = 1
             else:
@@ -16,97 +18,85 @@ def calc_best_tag(current_animals: list):
     dist_half = 100000
     tag_selected = 'unknown'
     half = len(current_animals) / 2
-    for key, value in tag_count.items():    
+    for key, value in tag_count.items():
         if abs(value - half) < dist_half:
             tag_selected = key
-            dist_half = abs(value - half) 
+            dist_half = abs(value - half)
 
     return tag_selected
 
 
-with open('anikator/database.yml', 'r') as ff:
+def yes_or_no(question):
+    while True:
+        answer = input("{} (s/n)?".format(question)).lower().strip()
+        if answer in ('s', 'n'):
+            break
+        print("Perdón, la respuesta solo puede ser 's' o 'n'")
+
+    return answer == 's'
+
+
+with open('database.yml', 'r') as ff:
     database = yaml.load(ff, Loader=SafeLoader)
 
 animals = database['animals']
 tags = database['tags']
+
+# transform tags into sets
+for key, value in animals.items():
+    value['tags'] = set(value['tags'])
+
 input("Hola! Piensa en un animal, y lo voy a intentar adivinar. Pulsa [ENTER] para continuar.")
 
 current = copy.deepcopy(animals)
 
 tag = calc_best_tag(current)
-positive_tags = []
+positive_tags = set()
 
 while tag != '':
-    has_tag = input("Tu animal tiene la caracteristica: {} (s/n)?".format(tag))
-    has_tag =  has_tag == 's'
+    has_tag = yes_or_no("Tu animal tiene la caracteristica: {}".format(tag))
 
     if has_tag:
-       positive_tags.append(tag) 
+        positive_tags.add(tag)
 
-    # Discard all the animals that don´t fulfil the condition
-    new_current = []
-    for animal in current:
-        curr_anim_has_tag = tag in animal['tags']
-        if curr_anim_has_tag == has_tag:
-            new_current.append(animal)
-            if curr_anim_has_tag:
-                animal['tags'].remove(tag)  
-    current = new_current
+    # Discard all the animals that don´t fulfil the condition with a comprehension dictionary
+    current = {key: value for key, value in current.items() if (tag in value['tags']) == has_tag}
 
     # If only one fulfils, we have our guess!
     if len(current) == 1:
-        animal_guessed = current[0]['name']
-        guessed = input("Tu animal es {} (s/n)?".format(animal_guessed))
+        animal_guessed = list(current.keys())[0]
+        guessed = yes_or_no("Tu animal es {}".format(animal_guessed))
         tag = ''
-    else: # If more than one fulfils, we need to ask more questions
-        tag_selected = calc_best_tag(current)
+    else:  # If more than one fulfils, we need to ask more questions
+        tag = calc_best_tag(current)
 
-        if tag_selected == 'unknown': # Jump to the pool
-            index = random.randrange(0, len(current) - 1)
-            animal_guessed = current[index]['name']
-            guessed = input("Humm.. no estoy seguro. Tu animal es {} (s/n)?".format(animal_guessed))
-            tag = ''
-        else:
-            tag = tag_selected
-
-if guessed == 's':
+if guessed:
     print("Soy la polla!")
 else:
-    new_animal_name = input("Cual era el animal en que estabas pensando?")
-    new_tag = input("Dime una caracteristica que me ayude a diferenciar entre {} y {}.".format(new_animal_name, animal_guessed))
-    has = input("{} tiene la caracteristica {}? (s/n)".format(new_animal_name, new_tag))
+    user_animal_name = input("Cual era el animal en que estabas pensando?")
+    differ_tag = input("Dime una caracteristica que me ayude a diferenciar entre {} y {}.".format(user_animal_name,
+                                                                                                  animal_guessed))
+    user_animal_has_tag = yes_or_no("{} tiene la caracteristica {}".format(user_animal_name, differ_tag))
 
-    if new_tag not in tags:
-        tags.append(new_tag)
+    if differ_tag not in tags:
+        tags.append(differ_tag)
 
-    already_exists = False
-    for animal in animals:
-        if animal['name'] == new_animal_name: 
-            if has == 's': 
-                without_duplicates = animal['tags']
-                without_duplicates.extend(positive_tags)
-                without_duplicates.append(new_tag)
-                animal['tags'] = list(set(without_duplicates))
-            else:
-                for animal2 in animals:
-                    if animal2['name'] == animal_guessed: 
+    if user_animal_name not in animals:
+        animals[user_animal_name] = {'tags': set()}
 
-                        animal2['tags'].append(new_tag)
-            # break comentar esto con noa. Hace falta añadir tambien en el else los positive tags
-    if has == 's':
-        new_animal = {'name': new_animal_name, 'tags': positive_tags}
-        new_animal['tags'].append(new_tag)
-        animals.append(new_animal)
+    if user_animal_has_tag:
+        animals[user_animal_name]['tags'].add(differ_tag)
     else:
-        for animal in animals:
-            if animal['name'] == animal_guessed: 
-                animal['tags'].append(new_tag)
-        new_animal = {'name': new_animal_name, 'tags': positive_tags}
-        animals.append(new_animal)
+        animals[animal_guessed]['tags'].add(differ_tag)
 
-    with open('anikator/database.yml', 'w') as ff:
+    animals[user_animal_name]['tags'].update(positive_tags)
+
+    shutil.copy('database.yml', 'database_bck.yml')
+
+    with open('database.yml', 'w') as ff:
+        # transform tags back into lists, so that yaml.dump works properly
+        for key, value in animals.items():
+            value['tags'] = list(value['tags'])
+
         output = yaml.dump(database)
         ff.write(output)
-
-
-
